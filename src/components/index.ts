0a46a9ebf159b436034186c6d9b87a02ce72731b
TanKaka123 <hongtan1422002@gmail.com>
2023-12-24 11:01:38 +0700

    [create] index component and carousel component

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Button from "./button";
+import Card from "./card";
+import CarouselDefault from "./carousel";
+import CarouselMultiple from "./carouselMultiple";
+import Input from "./input";
+import Text from "./text";
+
+export { Button, Card, CarouselDefault, CarouselMultiple, Input, Text };
