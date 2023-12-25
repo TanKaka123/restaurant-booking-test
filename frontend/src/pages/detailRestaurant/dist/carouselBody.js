d558be9d8f807c8ba7d1006e9cb58c5284f84b8f
TanKaka123 <hongtan1422002@gmail.com>
2023-12-25 14:58:06 +0700

    create restaurant page

---
+++ frontend/src/pages/detailRestaurant/dist/carouselBody.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var carousel_1 = require("../../components/carousel");
+var CarouselBody = function () {
+    return (React.createElement(React.Fragment, null,
+        React.createElement(carousel_1["default"], null)));
+};
+exports["default"] = CarouselBody;
