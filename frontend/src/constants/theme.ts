ff7ccb00fc7bced5d0950315a3ad63ada415c1c0
TanKaka123 <hongtan1422002@gmail.com>
2023-12-25 14:59:33 +0700

    create constant

---
+++ frontend/src/constants/theme.ts
@@ -0,0 +1,10 @@
+const colors = {
+    PRIMARY: "#FF2400",
+    SECONDARY: "#ff6b6b",
+    GRAY: "#808080",
+    WHITE: "#FFFFFF",
+    TRANSPARENT: "transparent",
+    BLACK:"#000000",
+  };
+  
+  export default colors;
